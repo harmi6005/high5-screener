8851f99c238e2a823d4d2407d141db157db0ff97
harmi6005 <lightsky7pm@naver.com>
2026-08-31 14:34:56 +0900

    Update full_scan_bithumb.py

--- scripts/full_scan_bithumb.py
+++ scripts/full_scan_bithumb.py
@@ -23,7 +23,7 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from common import (MAX_CHASE_RATIO, TOP_PICKS_COUNT, check_high5_system, notify_telegram,
                      send_long_message, build_watch_summary, pick_top_entries)
-from storage import save_scan_for_market, load_tracked
+from storage import save_scan_for_market, load_tracked, is_market_paused
 
 MAX_WORKERS = 10
 MARKET_LABEL = 'COIN'
@@ -93,6 +93,11 @@ def build_top_picks_message(top_entries, entry_cnt):
 
 
 if __name__ == "__main__":
+    if is_market_paused('COIN'):
+        print("코인 추적이 일시정지 상태라서 스캔을 건너뜁니다. "
+              "'코인 추적재시작' 명령으로 재개할 수 있어요.")
+        sys.exit(0)
+
     coins = get_registered_coins()
     if not coins:
         print("[코인 5일신고가] 관심등록된 코인이 없어 스캔을 건너뜁니다. "
